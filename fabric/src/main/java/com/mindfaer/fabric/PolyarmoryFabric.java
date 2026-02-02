fe40df21f3999034e77981876fc21f32e5f447b2
Trillium Sylvan <75921990+MindFaer@users.noreply.github.com>
2026-02-01 20:56:12 -0500

    init 1.20.1

--- fabric/src/main/java/com/mindfaer/fabric/PolyarmoryFabric.java
+++ fabric/src/main/java/com/mindfaer/fabric/PolyarmoryFabric.java
@@ -1,5 +1,6 @@
 package com.mindfaer.fabric;
 
+import com.mindfaer.Polyarmory;
 import net.fabricmc.api.ModInitializer;
 
 import com.mindfaer.Polyarmory;
@@ -7,6 +8,11 @@ import com.mindfaer.Polyarmory;
 public final class PolyarmoryFabric implements ModInitializer {
     @Override
     public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        // Run our common setup.
         Polyarmory.init();
     }
 }
